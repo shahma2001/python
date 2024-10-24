e308be1e6cc8b0ca8c99e8ce2be3a893cccd3e40
shahma2001 <24mcam02@meaec.edu.in>
2024-10-24 16:11:35 +0530

    Add files via upload

---
+++ mdict.py
@@ -0,0 +1,15 @@
+n1=int(input("how many items in dictionary 1?"))
+dict1={}
+for i in range(n1):
+    key=input("enter key:")
+    value=input("enter value:")
+    dict1[key]=value
+n2=int(input("items in dictionary 2?"))
+dict2={}
+for i in range(n2):
+    key=input("enter value:")
+    dict2[key]=value
+    merged_dict={**dict1,**dict2}
+    print("merged dictionary:",merged_dict)
+
+
